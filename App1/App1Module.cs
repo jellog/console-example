f0e41bc74372019ad06357985fdc9c107cd03fe0
Truong Nguyen <truong.nguyen@outlook.com>
2022-09-05 20:43:43 -0500

    initial

---
+++ App1/App1Module.cs
@@ -0,0 +1,28 @@
+﻿using DataGap.Jellog.Autofac;
+using DataGap.Jellog.EventBus.Kafka;
+using DataGap.Jellog.Kafka;
+using DataGap.Jellog.Modularity;
+
+namespace App1
+{
+    [DependsOn(
+        typeof(AbpEventBusKafkaModule),
+        typeof(AbpAutofacModule)
+        )]
+    public class App1Module : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<AbpKafkaOptions>(options =>
+            {
+                options.Connections.Default.BootstrapServers = "127.0.0.1:9092";
+            });
+
+            Configure<AbpKafkaEventBusOptions>(options =>
+            {
+                options.GroupId = "MyGroupId1";
+                options.TopicName = "MyTopicName";
+            });
+        }
+    }
+}
---
+++ App2/App2Module.cs
@@ -0,0 +1,28 @@
+﻿using DataGap.Jellog.Autofac;
+using DataGap.Jellog.EventBus.Kafka;
+using DataGap.Jellog.Kafka;
+using DataGap.Jellog.Modularity;
+
+namespace App2
+{
+    [DependsOn(
+        typeof(AbpEventBusKafkaModule),
+        typeof(AbpAutofacModule)
+        )]
+    public class App2Module : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<AbpKafkaOptions>(options =>
+            {
+                options.Connections.Default.BootstrapServers = "127.0.0.1:9092";
+            });
+
+            Configure<AbpKafkaEventBusOptions>(options =>
+            {
+                options.GroupId = "MyGroupId2";
+                options.TopicName = "MyTopicName";
+            });
+        }
+    }
+}
